de0081e61192251db54a6621a2d561290cae61f8
Dongyu Jiang <brook@r020080.bs.swh.mhn.de>
2020-02-27 00:06:13 +0800

    completed first part of lab1

---
+++ lab1/HellowNumbers.java
@@ -0,0 +1,17 @@
+public class HellowNumbers {
+
+    public static double Larger(double x,double y){
+        return Math.max(x,y);
+    }
+
+    public static void main(String[] args) {
+        /*
+        double x = 2.1;
+        int i = 1;
+        while(i <= 10) {
+            System.out.println(Math.pow(x,1+i));
+            i += 1;
+         */
+        System.out.println(Larger(Math.pow(23, 0.5),4.88));
+    }
+}
